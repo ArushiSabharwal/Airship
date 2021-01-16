8d6b425b8eb1c07fcdb7f175133aba1927486936
Arushi Sabharwal <45505654+ArushiSabharwal@users.noreply.github.com>
2021-01-15 18:45:43 -0800

    Add files via upload

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1610761418.069421
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'http://www.facebook.com',
+  defaultTitle: 'ArushiSabharwal\u002DInterviewCandidate',
+  defaultActionURL: 'http://www.google.com',
+  appKey: 'QrTKzroaRYe3b50SpCaLIQ',
+  token: 'MTpRclRLenJvYVJZZTNiNTBTcENhTElROjgwUGJYUlJJengxRE9tXzVTSm80azlFcTgzWWp6OFpBQTJna2o4ckN6ak0',
+  vapidPublicKey: 'BBC11WUhyEMRd5P6ctPnC9fXwBh5ryJRV8bsFPbQS-KbGFc8QA5emLCfOEN1ux2iSTUh9f487LDiWw20q-4UL4A='
+})
